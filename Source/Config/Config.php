1282859765d9703e4ffe24bd11309895e8c8ac93
Thomas Eiling <teiling88@gmail.com>
2019-04-12 14:24:08 +0200

    fix docker setup

--- Source/Config/Config.php
+++ Source/Config/Config.php
@@ -22,23 +22,20 @@ class Config extends \Shopware_Components_Config
     public function __construct(array $config, int $defaultShopId)
     {
         parent::__construct($config);
-        $this->customConfig = [];
-        if (isset($config['custom']['config'])) {
-            $this->customConfig = $config['custom']['config'];
-        }
+        $this->customConfig = $config['custom']['config'] ?? [];
 
         // support for Shopware()->Config()->getByNamespace('<pluginName>', '<configName>')
         if (isset($config['custom']['plugins'])) {
-            foreach($config['custom']['plugins'] as $shopId => $pluginConfigs) {
-                foreach($pluginConfigs as $pluginName => $pluginConfigValues) {
-                    foreach($pluginConfigValues as $pluginConfigName => $pluginConfigValue) {
-                        $pluginConfigKey = $pluginName."::".$pluginConfigName;
-                        $this->customConfig[(int)$shopId][$pluginConfigKey] = $pluginConfigValue;
+            foreach ($config['custom']['plugins'] as $shopId => $pluginConfigs) {
+                foreach ($pluginConfigs as $pluginName => $pluginConfigValues) {
+                    foreach ($pluginConfigValues as $pluginConfigName => $pluginConfigValue) {
+                        $pluginConfigKey = $pluginName . '::' . $pluginConfigName;
+                        $this->customConfig[(int) $shopId][$pluginConfigKey] = $pluginConfigValue;
                     }
                 }
             }
         }
-        
+
         $this->defaultShopId = $defaultShopId;
     }
 
@@ -48,11 +45,7 @@ class Config extends \Shopware_Components_Config
      */
     public function offsetGet($name)
     {
-        if (isset($this->customConfig[$this->getShopId()][$name])) {
-            return $this->customConfig[$this->getShopId()][$name];
-        }
-
-        return parent::offsetGet($name);
+        return $this->customConfig[$this->getShopId()][$name] ?? parent::offsetGet($name);
     }
 
     /**
